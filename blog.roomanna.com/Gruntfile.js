294291c20e05d409131ae02f54682aad2cf85308
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-03-31 13:57:57 -0700

    Additional Gruntfile build work.

--- blog.roomanna.com/Gruntfile.js
+++ blog.roomanna.com/Gruntfile.js
@@ -1,12 +1,43 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-
     pkg: grunt.file.readJSON('package.json'),
+
+    bgShell: {
+      ghostwriter: {
+        cmd: 'ghostwriter -src=src/site -dst=build/content',
+        bg: false,
+      },
+
+      create: {
+        cmd: 'ghostwriter -src=src/site -dst=build/content -action=create',
+        bg: false,
+      },
+
+      serve: {
+        cmd: '~/src/google_appengine_go/dev_appserver.py --port=9998 --address=0.0.0.0 build',
+        bg: true,
+      },
+
+      deploy: {
+        cmd: '~/src/google_appengine_go/appcfg.py --oauth2 update build',
+        bg: false,
+      },
+    },
 
     clean: ['build'],
 
     copy: {
-      dist: {
+      server: {
+        files: [
+          {
+            expand: true,
+            cwd:    'server',
+            src:    ['**/*'],
+            dest:   'build',
+          }
+        ],
+      },
+      frontend: {
         files: [
           {
             expand: true,
@@ -95,6 +126,7 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-bg-shell');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -103,5 +135,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['copy', 'less', 'concat', 'cssmin', 'uglify']);
+  grunt.registerTask('frontend', ['copy:frontend', 'less', 'concat', 'cssmin', 'uglify']);
+  grunt.registerTask('serve',    ['copy:server', 'bgShell:serve']);
+  grunt.registerTask('server',   ['copy:server']);
+  grunt.registerTask('create',   ['bgShell:create']);
+  grunt.registerTask('content',  ['bgShell:ghostwriter']);
+  grunt.registerTask('all',      ['server', 'frontend', 'content']);
+  grunt.registerTask('default',  ['all']);
 };
