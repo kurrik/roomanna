6794d78fa64942e3a2d58f8c42c27ad87fe9a64c
Arne Roomann-Kurrik <kurrik@gmail.com>
2016-05-05 10:54:03 -0700

    Holy mountain review

---
+++ blog.roomanna.com/src/site/posts/033-the-holy-mountain/keyframes.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+GITROOT=`git rev-parse --show-toplevel`
+POSTROOT=$GITROOT/blog.roomanna.com/src/site/posts/033-the-holy-mountain
+
+for video in `ls $POSTROOT/*.mp4`; do
+  echo "Generating poster for $video"
+  ffmpeg -i $video -ss 00:00:00.000 -vframes 1 ${video}.png
+done
