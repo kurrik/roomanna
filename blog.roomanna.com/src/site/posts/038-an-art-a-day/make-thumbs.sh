71dcaaf388d58e10331d231b3f6418ed7b8bc582
Arne Roomann-Kurrik <kurrik@gmail.com>
2017-03-17 16:43:42 -0700

    Make thumbnails

--- blog.roomanna.com/src/site/posts/038-an-art-a-day/make-thumbs.sh
+++ blog.roomanna.com/src/site/posts/038-an-art-a-day/make-thumbs.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# brew install imagemagick
+# http://www.imagemagick.org/Usage/thumbnails/
+
 GITROOT=`git rev-parse --show-toplevel`
 POSTROOT=$GITROOT/blog.roomanna.com/src/site/posts/038-an-art-a-day
 
-cd $POSTROOT
+mkdir -p $POSTROOT/thumbs
+cd $POSTROOT/images
 mogrify \
   -format jpg \
+  -path $POSTROOT/thumbs \
   -define jpeg:size=500x180 \
   -auto-orient \
   -thumbnail 250x90 \
